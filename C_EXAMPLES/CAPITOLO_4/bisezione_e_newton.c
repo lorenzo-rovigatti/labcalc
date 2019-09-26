7abc1c949797337e2b43f7eb057a9d55ff61c7f3
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-26 15:31:36 +0200

    esempio bisezione e newton completato

--- C_EXAMPLES/CAPITOLO_4/bisezione_e_newton.c
+++ C_EXAMPLES/CAPITOLO_4/bisezione_e_newton.c
@@ -1,15 +1,29 @@
 #include <math.h>
 #include <stdio.h>
-
-#define EPSILON 1.e-5
+#ifndef METODO
+#define METODO 0 // 0 = bisezione 1 = newton
+#endif
+#define EPSILON 1.0E-7
 
 int main(int argc, char** argv) {
-  double a = 0., b = M_PI, c;
-  double delta = fabs(a - b);
+  double delta=10.0*EPSILON;
+  double c;
+#if METODO==0
+  double a = 0., b = M_PI;
+#else
+  double dc;
+#endif
   int i = 1;
   printf("Questo programma trova la soluzione "
          "dell'equazione cos(x)=0 in (0, PI)\n");
+#if METODO==0
+  printf("Con il metodo della bisezione\n");
+#else
+  printf("Con il metodo di Newton\n");
+  c = M_PI/4.0;
+#endif
   while (delta > EPSILON) {
+#if METODO==0
     double p;
     c = 0.5 * (a + b);
     p = cos(a) * cos(c);
@@ -23,7 +37,13 @@ int main(int argc, char** argv) {
     printf("Iterazione n. %d: x = %f\n", i++, 
 	   0.5 * (a + b));
     delta = fabs(a - b);
-  }
+#else
+    dc = -cos(c)/sin(c);
+    c = c - dc;
+    printf("Iterazione n. %d: x = %f\n", i++, c);
+    delta = fabs(dc);
+#endif
+   }
   printf("La soluzione trovata vale: %f\n", c);
   printf("La soluzione vera vale   : %f\n", 0.5 * M_PI);
   printf("La differenza vale       : %e\n", 
