ea7520acbf9f85521eca9b5d6cf60bdcea09ad49
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-02 07:32:41 +0200

    spostato esempio 4.11 su errori arrotondamento

--- C_EXAMPLES/CAPITOLO_4/errore_arrotondamento_4_11.c
+++ C_EXAMPLES/CAPITOLO_4/errore_arrotondamento_4_11.c
@@ -3,23 +3,27 @@
 /* compilare con -D FORMOUT per discutere formati di output */
 /* compilare con -D LOCERR per mostrare la prima occorrenza dell'errore */
 #define N 10000000
-
 int main(int argc, char **argv) {
   float S = 0., x = 7.;
   unsigned int i, iS = 0, ix = 7;
-
-
-  for (i = 0; i < N; i++) {
-    S += x;
-   iS += ix;
 #ifdef LOCERR
-   if (S != iS)
-     {
-       printf("i=%d errore S=%.1f è diverso da iS=%d!\n", i, S, iS);
-       printf("%.1f + %.1f = %.1f\n", (float)(iS-x), x, (float)(iS-x) + x);
-       printf("16777222.0+7.0=%.1f\n", 16777222.0+7.0);
-       exit(0);
-     }
+  unsigned int cS, pS;
+#endif
+  for (i = 0; i < N; i++) 
+    {
+      S += x;
+      iS += ix;
+#ifdef LOCERR
+      cS = (unsigned int) S;// converto il float S ad un intero
+      // se il valore convertito a intero è diverso dal valore 
+      // esatto allora stampe ed esci
+      if (cS != iS)  
+        {
+          pS = iS - ix; // valore si S prima dell'errore di arrotondamento
+          printf("i=%u errore S=%.1f è diverso da iS=%u!\n", i, S, iS);
+          printf("Il motivo è che in virgola mobile: %.1f + %.1f = %.1f\n", (float) pS , x, (float) pS + x);
+          exit(0);
+        }
 #endif
   }
 
@@ -28,12 +32,12 @@ int main(int argc, char **argv) {
    * m = numero caratteri totali incluso il . (eventualmente aggiunge spazi a sinistra)
    * n = numero cifre dopo virgola 
    * */
-  printf("Usando float  : %12.1f x %12.1d = %12.1f\n", x, N, S); 
-  printf("Usando interi : %12d x %12d = %12d\n", ix, N, iS);
+  printf("Usando float  : %12.1f x %12d = %12.1f\n", x, N, S); 
+  printf("Usando interi : %12u x %12d = %12u\n", ix, N, iS);
 #ifdef FORMOUT
   printf("                123456789012 x 123456789012 = 123456789012\n"); 
-  printf("formato float:       %%12.1f x       %%12.1d =       %%12.1f\n"); // %% stampa un % 
-  printf("formato interi:        %%12d x       %%12d   =       %%12d\n");
+  printf("formato float:       %%12.1f x       %%12d =       %%12.1f\n"); // %% stampa un % 
+  printf("formato interi:        %%12u x       %%12d   =       %%12u\n");
   /* per maggiori dettagli dal terminale impartire il comando:
    * man printf
    */
