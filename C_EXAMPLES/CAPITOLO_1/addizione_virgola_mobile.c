6df99ff39c36126c77d5d0f6da3fa728fdbfdffd
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-03 13:53:38 +0200

    addizione_virgola_mobile.c: aggiunto esempio di conversione

--- C_EXAMPLES/CAPITOLO_1/addizione_virgola_mobile.c
+++ C_EXAMPLES/CAPITOLO_1/addizione_virgola_mobile.c
@@ -20,21 +20,29 @@ void printBits(size_t const size, void *ptr)
           c++;
         }
     }
-  //puts("");
 }
 int main(int argc, char **argv)
 {
-  float a=22.75, b=4.5, c;
-  c=a+b;
-  printf("%f + %f = %f\n\n", a, b, c);
-  printf("Rappresentazione in virgola mobile degli addendi (4 byte=32 bit)\n\n");
-  printf("Algoritmo conversione:\n");
-  printf("1) convertire il numero prima della virgola in binario e poi la parte frazionaria \n");
+  float a=22.75, b=4.5, c, d=1.327;
+  
+  printf("Algoritmo di conversione:\n");
+  printf("1) convertire il numero prima della virgola in binario e poi la parte frazionaria (moltiplicando per 2)\n");
   printf("22.75 = 10110.11 \n");
   printf("  4.5 =   100.1\n");
   printf("2) spostare la virgola fino ad avere la forma 1.XXX\n\n");
   printf("10110.11 -> 1.01101100000000000000000 x 2^4\n");
   printf("   100.1 -> 1.00100000000000000000000 x 2^2\n");
+  printf("Esempio di conversione di 1.327 in binario è:\n\n");
+  printBits(4, (void*)&d);
+  printf("\n\n");
+
+  printf("-----------------------------------------------\n");
+  printf("Esempio di somma di numeri in virgola mobile:\n");
+
+  c=a+b;
+  printf("\n%f + %f = %f\n\n", a, b, c);
+  
+  printf("Rappresentazione in virgola mobile degli addendi (4 byte=32 bit)\n\n");
   printf("\novvero in binario\n");
   printf("(bit del segno + 8 bit esponente + 23 bit mantissa)\n\n");
   printBits(4,(void*)&a);
