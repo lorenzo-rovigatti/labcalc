6d03f3ca9777c904c3abc5191b62afeef2094d7d
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-04 17:02:21 +0200

    esempi numeri random

---
+++ C_EXAMPLES/CAPITOLO_5/random_float_con_rand.c
@@ -0,0 +1,24 @@
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define FIXED_SEED 0
+int main(int arcg, char**argv)
+{
+  unsigned int seed;
+  int i;
+  double r;
+#if FIXED_SEED==1
+  seed = 200;
+#else
+  seed = time(0); // restituisce il tempo dal 01/01/1970 00:00:00 UTC in secondi
+                  // UTC=coordinated universal time 
+#endif
+  srand(seed);
+  printf("sequenza di 10 numeri casuali double tra 0 e 1:\n");
+  for (i = 0; i < 10; ++i) 
+    {
+      r=(double)rand()/RAND_MAX; 
+      printf("Ho generato il numero %18.16f\n", r);
+    }
+  return 0;
+}
---
+++ C_EXAMPLES/CAPITOLO_5/random_float_drand48.c
@@ -0,0 +1,24 @@
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define FIXED_SEED 0
+int main(int arcg, char**argv)
+{
+  int seed;
+  int i;
+  double r;
+#if FIXED_SEED==1
+  seed = 200;
+#else
+  seed = time(0); // restituisce il tempo dal 01/01/1970 00:00:00 UTC in secondi
+                  // UTC=coordinated universal time 
+#endif
+  srand48(seed);
+  printf("sequenza di 10 numeri casuali double tra 0 e 1:\n");
+  for (i = 0; i < 10; ++i) 
+    {
+      r=(double)drand48(); 
+      printf("Ho generato il numero %18.16f\n", r);
+    }
+  return 0;
+}
