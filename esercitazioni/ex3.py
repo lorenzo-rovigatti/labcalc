dc86e2c3287a783fa4227989cd4106a8e872122c
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-14 08:26:20 +0200

    cambiamenti da nicoletta

--- esercitazioni/ex3.py
+++ esercitazioni/ex3.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import numpy as np
-plt.title('Un primo plot con Python')
+plt.title('Sin(x)/x')
 plt.xlabel('x')
 plt.ylabel('y')
-x = np.linspace(0.0, 5.0, 100)
-y = x
+x = np.linspace(-15.0, 15.0, 2)
+y = 0.0*x
 plt.plot(x,y,label='y=x')
-x, y = np.loadtxt('temp.dat', usecols=(0,2), delimiter=' ', unpack=True)
-plt.plot(x,y, 'x',label='Loaded from file!')
-plt.savefig('traiettoria.png')
+x, y = np.loadtxt('sinxx.dat', usecols=(0,1), delimiter=' ', unpack=True)
+plt.plot(x,y, '-x',label='Loaded from file!')
+plt.savefig('sinx.png')
 plt.show()
