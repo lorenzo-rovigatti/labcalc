50112f6e3b205accfdedf3cea27775939095e4d3
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-16 12:24:46 +0200

    aggiunto file d'esempio scritto a lezione

---
+++ C_EXAMPLES/CAPITOLO_2/prog_modified.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+#include<float.h>
+int main(void) {
+int a; //intero con segno a 32 bit
+float b; // floating point 32 bit
+double c; // fp a 64 bit
+char d='c';
+long long int e; // intero con segno a 64 bit
+unsigned int ua0;
+unsigned char uc; // unsigned char
+long double lc; // 96 bit fo
+printf ("sqrt(9)=%f\n", 
+        sqrt(9.0));
+return 0;
+}
