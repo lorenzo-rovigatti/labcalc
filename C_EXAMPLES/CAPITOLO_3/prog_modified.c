ba1f18df39a1ee2d6052ed3d539255f8b05fbe4f
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-17 12:37:18 +0200

    upd

--- C_EXAMPLES/CAPITOLO_3/prog_modified.c
+++ C_EXAMPLES/CAPITOLO_3/prog_modified.c
@@ -1,22 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 int main(void) {
-int a; //intero con segno a 32 bit
-float b; // floating point 32 bit
-double c; // fp a 64 bit
-char d='c';
+int a=2;
+double c=2.0/3.0, c2; // fp a 64 bit
+char d=-3;
 char d2;
-long long int e; // intero con segno a 64 bit
-unsigned int ua0;
-unsigned char uc; // unsigned char
-long double lc; // 96 bit fo
-printf ("sqrt(9)=%f\n", 
-        sqrt(9.0));
-/* operatori unari ++ e -- */
 d2='b';
-a=1;
-printf("b=%d\n", a++);
-a=1;
-printf("b=%d\n", ++a);
+c2=2.0+c;
+printf("un intero occupa %lu\n", sizeof(char));
+a=(unsigned int)d;
+printf("ciao %i\n", a);
 return 0;
+printf("immetti un intero:");
+scanf("%d",&a);
+printf("hai immesso %d\n", a);
 }
