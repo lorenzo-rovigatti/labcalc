46e5c89e482c64c41e8185896b420a26a935916a
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-17 11:09:02 +0200

    upd

---
+++ C_EXAMPLES/CAPITOLO_3/prog_modified.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main(void) {
+int a; //intero con segno a 32 bit
+float b; // floating point 32 bit
+double c; // fp a 64 bit
+char d='c';
+char d2;
+long long int e; // intero con segno a 64 bit
+unsigned int ua0;
+unsigned char uc; // unsigned char
+long double lc; // 96 bit fo
+printf ("sqrt(9)=%f\n", 
+        sqrt(9.0));
+/* operatori unari ++ e -- */
+d2='b';
+a=1;
+printf("b=%d\n", a++);
+a=1;
+printf("b=%d\n", ++a);
+return 0;
+}
