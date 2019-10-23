8f8fd93a3317bf1355c7f494029781540f99f3e5
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-23 09:45:06 +0200

    aggiunti commenti

--- C_EXAMPLES/CAPITOLO_4/errore_arrotondamento_track_errors.c
+++ C_EXAMPLES/CAPITOLO_4/errore_arrotondamento_track_errors.c
@@ -36,7 +36,7 @@ int main(int argc, char **argv) {
 
   /* stampa riepilogo errori */
   sume = 0; 
-  printf("errore totale (volte x errore, errore ad ogni incremento di 7) = ");
+  printf("errore totale (volte x errore, errore ad ogni incremento di 7) =\n");
   for (i = 1; i < NM; i++)
     {
       if (errori[i]!=0)
@@ -54,7 +54,7 @@ int main(int argc, char **argv) {
         }
     } 
   printf(" = %u\n", sume); 
-  printf("\nsomma (volte x differenza, la differenza è 7 se l'errore è 0) = ");
+  printf("\nsomma (volte x differenza, la differenza è 7 se l'errore è 0) =\n");
   first=1;
   for (i = 1; i < NM; i++)
     {
