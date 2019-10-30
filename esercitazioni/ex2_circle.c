c3c98bf6d2321fb7692b94ea6dbd9721631b2e94
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-30 09:48:27 +0100

    aggiunta quarta parte a codice c

--- esercitazioni/ex2_circle.c
+++ esercitazioni/ex2_circle.c
@@ -3,10 +3,14 @@
 
 int main(void)
 {
+#ifdef QUARTA_PARTE
+  double vx, vy;
+#endif
   double R, w, t, x, y;
   R = 6.2; /* m */
   w = 0.1; /* rad/s */
-  t = 0.0; /* s */
+#ifndef INTERATTIVO
+   t = 0.0; /* s */
   x = R * cos(w*t);
   y = R * sin(w*t);
   printf("t = %.4f x = %.5f y = %.5f\n", t, x, y);
@@ -18,15 +22,23 @@ int main(void)
   x = R * cos(w*t);
   y = R * sin(w*t);
   printf("t = %.4f x = %.5f y = %.5f\n", t, x, y);
-  t = 10; /* s */
+  t = 20; /* s */
   x = R * cos(w*t);
   y = R * sin(w*t);
   printf("t = %.4f x = %.5f y = %.5f\n", t, x, y);
+#else
   /* blocco interattivo */
   printf("Immetti il tempo t: ");
   scanf("%lf", &t);
   x = R * cos(w*t);
   y = R * sin(w*t);
-  
+#ifdef QUARTA_PARTE
+  vx = -w*R*sin(w*t);
+  vy = w*R*cos(w*t); 
+  printf("%.5f %.5f %.5f %.5f %.5f\n", t, x, y, vx, vy);
+#else
   printf("%.5f %.5f %.5f\n", t, x, y);
+#endif
+#endif
 }
+
