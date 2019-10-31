0ed0bc7a2d51b5ff5f19163992d92d5d07df3b99
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-31 10:41:55 +0100

    minor change

--- C_EXAMPLES/CAPITOLO_5/array_2d.c
+++ C_EXAMPLES/CAPITOLO_5/array_2d.c
@@ -9,11 +9,12 @@ int main()
   int data2[N][M] = {{1,2},{3,4},{5,6}};
   int data3[][M] = {{11,22},{33,44},{55,66}};
   //
-  // inizializzazion con ciclo
+  // inizializzazion con doppio ciclo
   for (i=0; i < N; i++)
     for (j=0; j < M; j++)
       data[i][j] = 3.0; 
   printf("\n data[]=\n");
+
   for (i=0; i < N; i++)
     {
       for (j=0; j < M; j++)
