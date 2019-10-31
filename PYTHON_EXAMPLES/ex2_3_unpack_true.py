6db332fde998cd583b56bf1269907709ee774f95
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-10-31 10:32:17 +0100

    python examples

---
+++ PYTHON_EXAMPLES/ex2_3_unpack_true.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.title('Traiettoria')
+plt.xlabel('x')
+plt.ylabel('y')
+m= np.loadtxt('temp.dat', comments=['#'], usecols=(1,2), unpack=False)
+print('data=',m)
+x,y=m
+plt.plot(x, y, 'x',label='Traiettoria')
+plt.savefig('traiettoria.png')
+plt.show()
+
