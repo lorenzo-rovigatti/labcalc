00fe6ffcfe9195804c9ce3946503c087cb9c5004
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-11-07 18:33:51 +0100

    soluzione completa addendum

--- esercitazioni/alternative_ed_integrazioni/ex2_addendum.c
+++ esercitazioni/alternative_ed_integrazioni/ex2_addendum.c
@@ -4,16 +4,12 @@ int main(void)
 {
   const double g = 9.82; /* m/s^2 */ 
   double alpha, G, y, L, v0;
-#ifndef PARTE_II
   printf("Immetti v0: ");
-#endif
   scanf("%lf", &v0);
-#ifndef PARTE_II
   printf("Immetti alpha: ");
-#endif
   scanf("%lf", &alpha);
-#ifndef PARTE_II 
   printf("Hai immesso:\n   v0=%12.7f\nalpha=%12.7f\n", v0, alpha);
+#ifndef PARTE_II 
   printf("Immetti L: ");
   scanf("%lf", &L);
   y = L*tan(alpha) - g*L*L / (2.0*v0*v0*cos(alpha)*cos(alpha));
