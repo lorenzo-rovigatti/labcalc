7f53d615d764c8d44a49c6a41839d6d42b5a9b14
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-11-07 18:01:18 +0100

    soluzione completa ex3

--- esercitazioni/ex3_zero.c
+++ esercitazioni/ex3_zero.c
@@ -1,10 +1,9 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-#define PRINT_MESH
 int main(int argc, char** argv)
 {
-  int i, N=100, nzeri=0;
+  int i, N=6, nzeri=0;
   double xbest, xerr, s,fxL, fxR, xL, xR, dx, xmin=-15.0, xmax=15.0;
   xL=xmin;
   dx = (xmax-xmin)/N;
@@ -23,19 +22,22 @@ int main(int argc, char** argv)
       xbest=(xL+xR)/2.0;
       xerr=(xR-xL)/2.0;
 #ifdef PRINT_MESH
-      printf("%f 0.0\n", xR);
+      printf("%f 0.0\n", xL);
 #endif
       if (s <= 0)
         {
 #ifndef PRINT_MESH
-          printf("Trovato uno zero x=%12.7f +- %9.7f\n", (xL+xR)/2.0, xerr);
+          printf("Trovato uno zero x=%12.7f +- %9.7f\n", xbest, xerr);
 #endif
           nzeri += 1.0;
         }
       xL+=dx;
     }
+
 #ifndef PRINT_MESH
   printf("Numero zeri trovati=%d\n", nzeri);
+#else
+      printf("%f 0.0\n", xR);
 #endif
   return 0;
 }
