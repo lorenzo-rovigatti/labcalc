985f48ccc1272585778370fce8b7c3aee1721a43
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-11-13 08:52:50 +0100

    removed fprintf

--- esercitazioni/ex3_sinxx.c
+++ esercitazioni/ex3_sinxx.c
@@ -6,8 +6,6 @@ int main(int argc, char** argv)
   int i, Np=100;
   double fx, x, dx, sum=0.0, xmin=-15.0, xmax=15.0;
   x=xmin;
-  fprintf(stderr,"0/0=%.15G\n", sin(0.0)/0.0);
-  fprintf(stderr,"1/0=%.15G\n", 1.0/0.0);
   dx = (xmax-xmin)/Np;
   for (i=0; i < Np; i++)
     {
