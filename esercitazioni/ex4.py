0b7ce2f6a75f332da4c4e676724dfe20893e7efd
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-11-14 10:20:00 +0100

    python script unused var

--- esercitazioni/ex4.py
+++ esercitazioni/ex4.py
@@ -4,8 +4,8 @@ import numpy as np
 plt.title('Esercizio 4')
 plt.xlabel('x')
 plt.ylabel('y')
-n_bins=36
-x, y = np.loadtxt('dati.dat', usecols=(0,1), delimiter=' ', unpack=True)
+#n_bins=36
+x, y = np.loadtxt('isto100000000.dat', usecols=(0,1), delimiter=' ', unpack=True)
 plt.bar(x,y,fill=True)
 plt.savefig('isto1.png')
 #plt.clf()
