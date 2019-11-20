d495ca21e637eef2e520a8dc733765a68194c648
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-11-20 08:56:30 +0100

    updates

--- C_EXAMPLES/CAPITOLO_6/attenzione_mod_rand.c
+++ C_EXAMPLES/CAPITOLO_6/attenzione_mod_rand.c
@@ -11,16 +11,17 @@ int main(int argc, char *argv[])
   for (int i = 0; i < 2; i++) {
     nocc[i]=0;
   }
-  printf("nmax=%u verifico che le probabilità di ottenere\n")
-  printf("un numero minore o maggiore di %u/2=%u siano uguali\n ", 
-         nmax, nmax, nmax/2);
+  printf("Genero numero casuali tra 1 e %u\n", nmax); 
+  printf("e verifico che le probabilità di ottenere\n");
+  printf("un numero minore o maggiore di %u/2=%u siano uguali\n", 
+         nmax, nmax/2);
   for (i = 0; i < N; ++i) {
 #ifdef NO_USE_MOD
     n=nmax*((double)rand()/(RAND_MAX+1.0))+1; 
 #else
     n=rand()%nmax + 1;
 #endif
-    if (n < nmax/2.0)
+    if (n <= nmax/2.0)
       nocc[0] += 1;
     else 
       nocc[1] += 1;
