1164dbe43b2fddb38b9dce15d218f5209c40f22c
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-11-26 20:36:48 +0100

    aggiunta soluzione ex5 parte III

---
+++ esercitazioni/ex5_soluzione_partIII.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define NC 10 
+#define NTRIM 4
+int main(void)
+{
+  int n, i,j,dumb, voto[NC][NTRIM], dati[NC][9]; // colonna 0 vendite; colonna 1 ore
+  double media[NC][NTRIM],sortmedia[NC][2],temp1,temp2;
+  FILE *fpr;
+  char fn[255];
+  for (i=0; i < NC; i++)
+    {
+      for (j=0; j < NTRIM; j++)
+        {
+          media[i][j] = 0.0;
+          voto[i][j] = 0.0;
+        }
+      sortmedia[0][i] = sortmedia[1][i] = 0.0;
+    }
+
+  fpr=fopen("vendite2016.dat","r");
+  for (i=0;i<NC;i++)
+    {
+      fscanf(fpr, "%i ", &dumb);  
+      for (n=0; n < NTRIM; n++)
+        {
+          fscanf(fpr,"%i %i",&dati[i][1+2*n],&dati[i][1+2*n+1]);
+          media[i][n]=((double)dati[i][1+2*n])/dati[i][1+2*n+1];
+        }
+   }  
+
+  fclose(fpr);
+  /* Ordino i commessi in ordine di produttivita' */
+
+  printf("ordino\n");
+
+  for (n=0; n < NTRIM; n++)
+    {
+      for (i=0; i < NC; i++)
+        {
+          sortmedia[i][1]=media[i][n];
+          sortmedia[i][0]=(double)(i+1); // voglio che i commessi vadano da 1 a 10 quindi sommo 1       
+        }
+      /* BUBBLE SORT...
+       * dopo ogni ciclo in i ho nella posizione i-esima l'elemento più grande di tutti
+       * Il numero di confronti dunque è N-2 + N-1 + ... 1 = N*(N-1)/2 = O(N^2/2)  */
+      for (i=0;i<NC-1;i++) // mi fermo a NC-2 poiché j parte da NC - 1 quindi se i=NC-1 il ciclo in j
+        // non verrebbe effettuato
+        {
+          for (j=NC-1; j > 0; j--)//j=NC-1 è l'elemento più in "basso" 0 quello più in "alto"
+            // arrivo fino a i, infatti 
+            // 1) dopo il primo ciclo in 0 ho sicuramente l'elemento 
+            //    più grande quindi è inutile fari confronti con l'elemento in 0
+            // 2) dopo il secondo ciclo in 0 e 1 ho i due elementi più grandi 
+            //    e quindi è inutile confrontarsi con loro
+            {
+              if(sortmedia[j][1]>=sortmedia[j-1][1])
+                {
+                  temp1=sortmedia[j-1][0];
+                  temp2=sortmedia[j-1][1];
+                  sortmedia[j-1][0]=sortmedia[j][0];
+                  sortmedia[j-1][1]=sortmedia[j][1];
+                  sortmedia[j][0]=temp1;
+                  sortmedia[j][1]=temp2;
+                }
+            }
+        }
+
+      for (i=0;i<NC;i++)
+        {
+          temp1=sortmedia[i][0];
+          j=(int)(temp1-1); // i commessi vanno da 1 a 10 quindi devo sottrarre 1
+          voto[j][n]=NC-i;
+        }
+    }
+  // stampo i commessi con i voti
+  for (j=0;j<NC;j++)
+    {
+      sprintf(fn,"medie_commesso_N%d.dat", j+1);
+      fpr=fopen(fn, "w");
+      for (n=0; n < NTRIM; n++)
+        fprintf(fpr,"%3i %7.2f\n",n,media[j][n]);
+      fclose(fpr);
+      sprintf(fn,"voti_commesso_N%d.dat", j+1);
+      fpr=fopen(fn, "w");
+      for (n=0; n < NTRIM; n++)
+        fprintf(fpr,"%3i %3i\n",n,voto[j][n]);
+      fclose(fpr);
+    }
+}
