c22a367790388cab283f2110f07a88bb10202a7f
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-11-26 20:55:35 +0100

    python ex5

---
+++ esercitazioni/ex5_voti.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import numpy as np
+plt.title('Esercizio 5')
+plt.xlabel('trimestri')
+plt.ylabel('voti')
+for cc in range(1,11):
+    x,y = np.loadtxt('voti_2016.dat', usecols=(0,cc), unpack=True)
+    plt.plot(x, y, label='venditore #'+str(cc))
+plt.xticks(np.arange(4),('T1','T2','T3', 'T4'))
+plt.legend()    
+plt.savefig('voti_2016.png')
+plt.show()
