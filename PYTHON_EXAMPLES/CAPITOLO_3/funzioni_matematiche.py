6bd426992f31b68c67447e8b2da7ed4c25d6c612
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-12-02 16:56:52 +0100

    nuovi esempi in python

--- PYTHON_EXAMPLES/CAPITOLO_3/funzioni_matematiche.py
+++ PYTHON_EXAMPLES/CAPITOLO_3/funzioni_matematiche.py
@@ -3,4 +3,8 @@ a=float(input('Immetti un numero: '))
 print('sin('+str(a)+')=',np.sin(a))
 print('cos(',a,')=',np.cos(a))
 print('sqrt(',a,')=',np.sqrt(a))
-print('exp({:.3f})={:8.5f} ln({:.3f})={:8.5f}'.format(a,np.exp(a),a,np.log(a)))
+#il numero prima dei due punti indica quale argomento stampare tra quelli
+#forniti con format(...)
+#se si omette la prima stringa di forattazione {...} corrisponde al primo 
+#argomento di format, la seconda al secondo e così via.
+print('exp({2:.3f})={1:8.5f} ln({0:.3f})={3:8.5f}'.format(a,np.exp(a),a,np.log(a)))
