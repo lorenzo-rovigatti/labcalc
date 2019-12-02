b8c562cce38bfcdbb28e8aa2d5bb27df771ff158
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-12-02 17:00:37 +0100

    nuovi esempi in python

--- PYTHON_EXAMPLES/CAPITOLO_3/funzioni_matematiche.py
+++ PYTHON_EXAMPLES/CAPITOLO_3/funzioni_matematiche.py
@@ -8,3 +8,4 @@ print('sqrt(',a,')=',np.sqrt(a))
 #se si omette la prima stringa di forattazione {...} corrisponde al primo 
 #argomento di format, la seconda al secondo e così via.
 print('exp({2:.3f})={1:8.5f} ln({0:.3f})={3:8.5f}'.format(a,np.exp(a),a,np.log(a)))
+print('7/3={:3d}'.format(int(7/3)))
