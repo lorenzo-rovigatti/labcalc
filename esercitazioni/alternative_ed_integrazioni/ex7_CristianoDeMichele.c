faccfe1159667b57d32f981a5f6d487557168bb8
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-12-05 18:03:21 +0100

    ex7 cdm

--- esercitazioni/alternative_ed_integrazioni/ex7_CristianoDeMichele.c
+++ esercitazioni/alternative_ed_integrazioni/ex7_CristianoDeMichele.c
@@ -4,243 +4,120 @@
 #include <time.h>
 #define NMAX 20
 
-/************************************************************/
-/****************DICHIARAZIONE DI FUNZIONI*******************/
-/*tipo nome_funzione (TIPI DEI VALORI IN IN INPUT);*/
+// dichiarazioni di funzioni
+void inserimento(char *msg, char *fmt, void *v);
+int singolo_getto(void);
+void morra(void);
 
-void PrintInitMessage(void);
-void inserimento(char *message, char *format, void *var);
-void Init_punteggio(int vittoria[][NMAX]);
-int getto(void);
-void morra(int vittoria[][NMAX],int mano);
-void Scrivi_punteggio(int vittoria[][NMAX], int n_getti);
-char convert_into_string(int value);
-
-/************************************************************/
 /************************MAIN*******************************/
-
-int main(void){
-  int i,n,check=0;
-  int points[2][NMAX];
+int main(void)
+{
+  int s,i,n,fine, Ngetti;
+  int punteggi[NMAX][2];
+  FILE *f;
+  /* inzializzazione sequenza di numeri pseudo-casuali */
   srand48(time(NULL));
 
   //********** STAMPA MESSAGGIO INIZIALE E LETTURA DATI ***********
-  PrintInitMessage();
-  do{
-    inserimento("Inserisci il numero di getti (massimo n. consentito e' 20): ", "%i", &n);
-    if(n>0 && n<=NMAX){
-      check=1;
-    }else{
-      fprintf(stderr,"Attenzione! Il n. di getti non puo' essere negativo o maggiore di %i\n",NMAX);
+  printf("Simulazione del gioco della morra cinese (giocatore 0 vs giocatore 1)\n");
+  do
+    {
+      inserimento("Inserisci il numero di getti (massimo n. consentito e' 20): ", "%i", &Ngetti);
+      if(Ngetti>0 && Ngetti<=NMAX)
+        {
+          fine =1;
+        }
+      else
+        {
+          fine = 0;
+          printf("[ERRORE] Il n. di getti deve essere positivo e minore o uguale a %i\n",NMAX);
+        }
     }
-  }while(!check);
-
+  while(!fine);
 
   //************** SIMULAZIONE DELLA MORRA CINESE ***************  
-  Init_punteggio(points);
-    for(i=0;i<n;i++){
-      morra(points,i);
+  for (i=0; i < Ngetti; i++)
+    {
+      for (n = 0; n < 2; n++) 
+        {
+          punteggi[i][n] = 0;
+        }
     }
-  
+  for (i=0; i < Ngetti; i++)
+    {
+      if ((s=singolo_getto()) > 0)
+        punteggi[i][s]++;
+      printf("Getto #%d puntteggo di 0:%d punteggio di 1:%d\n", i, punteggi[i][0], punteggi[i][1]);
+    }
+
 
   //**************** STAMPA PUNTEGGIO SU FILE *******************
-  Scrivi_punteggio(points, n);
-  
-
-  return(0);
+  printf("0 ha totalizzato %d punti\n", punteggi[Ngetti-1][0]);
+  printf("1 ha totalizzato %d punti\n", punteggi[Ngetti-1][1]);
+  if (punteggi[Ngetti-1][0] > punteggi[Ngetti-1][1])
+    printf("Ha vinto il giocatore 0\n");
+  else if (punteggi[Ngetti-1][0] < punteggi[Ngetti-1][1])
+    printf("Ha vinto il giocatore 1\n");
+  else
+    printf("E' finita in parità\n");
+  f=fopen("punteggio.dat","w"); 
+  for (i=0; i < Ngetti; i++)
+    {
+      fprintf(f,"%d %d %d\n", i, punteggi[i][0], punteggi[i][1]);
+    }
+  fclose(f);
+  return 0;
 }
 
-/************************************************************/
-/**************DEFINIZIONE DI FUNZIONE***********************/
-/****************(PROTOTIPI DI FUNZIONI)*********************/
-
-/*tipo nome_funzione (tipo var1, tipo var2,....){
-  
-  blocco del codice
-}*/
-
-
-
-void PrintInitMessage(void){
-  
-  fprintf(stdout,"\n");
-  fprintf(stdout,"************************************************************\n");
-  fprintf(stdout,"QUESTO PROGRAMMA SIMULA IL GIOCO DELLA MORRA CINESE\n");
-  fprintf(stdout,"RICHIEDENDO IN INPUT IL NUMERO DI GETTI DA SIMULARE\n");
-  fprintf(stdout,"************************************************************\n");
-  fprintf(stdout,"\n");
-  fprintf(stdout,"------------------------------------------------------------\n");
-  fprintf(stdout,"***************************LEGENDA:*************************\n");
-  fprintf(stdout,"C=CARTA\nF=FORBICI\nS=SASSO\n");
-  fprintf(stdout,"------------------------------------------------------------\n");
-  fprintf(stdout,"\n");
+/* 1 = CARTA; 2 = SASSO; 3 = FORBICI */ 
+int genera_segno(void)
+{
+  return drand48()*3+1;
 }
-
-//Questa funzione legge un solo dato il cui tipo e' un parametro di
-//input della funzione (viene passato attraverso la stringa format) e controlla
-//che la lettura sia andata a buon fine e che non ci siano
-//cose rimaste nel buffer. Da notare che sto usando un puntatore
-//di tipo void perche' a priori non so qual'e' il tipo
-//della variabile che devo leggere.
-void inserimento(char *message, char *format, void *var){
-  int fine, res=0, c;
+int singolo_getto(void)
+{
+  int segno0, segno1, vincitore;
+  segno0 = genera_segno();
+  segno1 = genera_segno();
+  if (segno0==1 && segno1==2)      // carta vince su sasso
+    vincitore=0;
+  else if (segno0==2 && segno1==1)
+    vincitore=1;
+  else if (segno0==3 && segno1==1) // forbici vincono su carta
+    vincitore=0;
+  else if (segno0==1 && segno1==3)
+    vincitore=1;
+  else if (segno0==2 && segno1==3)  //sasso vince su forbici
+    vincitore=0;
+  else if (segno0==3 && segno1==2)
+    vincitore=1;
+  else 
+    vincitore=-1; // pari
+  return vincitore;
+}
+void inserimento(char *msg, char *fmt, void *v)
+{
+  int fine, res, c;
   fine=0;
-  char junk[80];
-  //finche' res non restituisce 1
-  while (!fine){
-    fprintf(stdout,"%s",message);
-    fine=1;
-    res=fscanf(stdin,format,var);
-    
-    if(res!=1){
-      fprintf(stderr,"Problema nella lettura da schermo\n");
-      fine=0;
+  while (!fine)
+    {
+      printf("%s", msg);
+      fine=1;
+      res=scanf(fmt, v);
+      if (ferror(stdin)||feof(stdin))
+        {
+          // qui ci arriva se si preme CTRL-D = EOF
+          printf("\n");
+          clearerr(stdin);
+          fine=0;
+          continue;
+        }
+          
+      if (res < 1)
+        {
+          printf("Problema nella conversione\n");
+          fine=0;
+        }
+      while ((c=getchar())!='\n'); // svuota il buffer di tutti i caratteri e termina con '\n'=newline
     }
-    if(fgets(junk, 80, stdin) != NULL && (junk[0] !='\n')){//se e' rimasto qualcosa nel buffer che non e' il carattere di ritorno
-      
-      fprintf(stderr,"c'e' qualcosa che e' rimasto nel buffer: ");
-      fprintf(stderr,"%s\n", junk);
-      fine=0;
-    }
-  }
-}
-
-void Init_punteggio(int vittoria[][NMAX]){
-  int i;
-  
-  for(i=0;i<NMAX;i++){
-    vittoria[0][i]=0;
-    vittoria[1][i]=0;
-  }
-  
-}
-//funzione che restituisce un n. intero tra [1,3] )
-int getto(void){
-  return((int)(drand48()*3)+1);
-}
-
-//funzione principale della simulazione
-void morra(int vittoria[][NMAX],int mano){
-  int value_A;
-  int value_B;
-  int PUNTOA=0,PUNTOB=0;
-  int diff=0;
-  
-  do{
-    value_A=getto();
-    value_B=getto();
-    diff=value_A-value_B;
-  }while(!diff);
-  
-  /*per cercare il vicitore ho pensato alla seguente strategia: assegno a
-    CARTA(C)   il n. 1
-    FORBICI(F) il n. 2
-    SASSO(S)   il n. 3
-    (quindi estraggo un numero intero tra 1 e 3)
-    Se sottraggo il valore estratto del giocatore B a quello del giocatore A trovo che
-    
-    A   B   diff      RISULTATO
-    ----------------------------------
-    C   S   1-3=-2    VITTORIA di A
-    C   F   1-2=-1    PERDITA  di A
-    S   C   3-1= 2    PERDITA  di A
-    S   F   3-1= 1    VITTORIA di A   
-    F   C   2-1= 1    VITTORIA di A
-    F   S   2-3= -1   PERDITA  di A
-    
-    quindi significa che ogni volta che diff=-2 o 1 allora ha vinto il giocatore A
-    se invece diff=2 o -1 ha vinto il giocatore B
-    SE I SEGNI DI A e B SONO UGUALI (es. CARTA-CARTA, FORBICI-FORBICI,SASSO-SASSO)
-    si effettua di nuovo un getto senza considerare la condizione di parita' 
-  */
-  
-  if((diff==-2) || (diff==1)){//VITTORIA DI A
-    PUNTOA=1;
-    PUNTOB=0;
-    if(mano>0){
-      vittoria[0][mano]=vittoria[0][mano-1]+PUNTOA;
-      vittoria[1][mano]=vittoria[1][mano-1]+PUNTOB;
-    }else{
-      vittoria[0][mano]=PUNTOA;
-      vittoria[1][mano]=PUNTOB;
-    }
-    
-  }
-  
-  if((diff==-1) || (diff==2)){//VITTORIA DI B
-    PUNTOA=0;
-    PUNTOB=1;
-    if(mano>0){
-    vittoria[0][mano]=vittoria[0][mano-1]+PUNTOA;
-    vittoria[1][mano]=vittoria[1][mano-1]+PUNTOB;
-    }else{
-      vittoria[0][mano]=PUNTOA;
-      vittoria[1][mano]=PUNTOB;
-    }
-  }
-  //volendo posso farmi stampare i risultati di ogni getto (si puo' commentare questa parte)
-  fprintf(stdout,"GETTO N. %i\tSEGNI %c %c\tPUNTI %i %i\tPUNTEGGIO PARZIALE %i %i\n",mano,convert_into_string(value_A),convert_into_string(value_B),PUNTOA,PUNTOB,vittoria[0][mano],vittoria[1][mano]);
-}
-
-/*stampa la serie temporale dei punteggi ottenuti in un file*/
-void Scrivi_punteggio(int vittoria[][NMAX], int n_getti){
-  FILE *fp;
-  int i;
-  int TotalA=vittoria[0][n_getti-1],TotalB=vittoria[1][n_getti-1];
-  fp=fopen("punteggio.dat","w");
-  
-  for(i=0;i<n_getti;i++){
-    fprintf(fp,"%i %i %i\n",i+1,vittoria[0][i],vittoria[1][i]);    
-  }
-  fclose(fp);
-
-
-  
-  if(TotalA>TotalB){
-    fprintf(stdout,"\n");
-    fprintf(stdout,"******************************************************************\n");
-    fprintf(stdout,"IL VINCITORE DELLA PARTIRA E' IL GIOCATORE A con %i VITTORIE SU %i\n",TotalA,n_getti);
-    fprintf(stdout,"******************************************************************\n");
-    fprintf(stdout,"\n");
-  }else{
-   if(TotalA==TotalB){
-     fprintf(stdout,"\n");
-     fprintf(stdout,"***********************************************************************\n");
-     fprintf(stdout,"I DUE GIOCATORI HANNO OTTENUTO LO STESSO PUNTEGGIO DI %i VITTORIE su %i\n",TotalA,n_getti);
-     fprintf(stdout,"**********************************************************************\n");
-    fprintf(stdout,"\n");
-   }else{
-     fprintf(stdout,"\n");
-     fprintf(stdout,"******************************************************************\n");
-     fprintf(stdout,"IL VINCITORE DELLA PARTIRA E' IL GIOCATORE B con %i VITTORIE SU %i\n",TotalB,n_getti);
-     fprintf(stdout,"******************************************************************\n");
-     fprintf(stdout,"\n");
-   }
-  }
-  fprintf(stdout,"La serie temporale dei punteggi e' stata scritta nel file punteggio.dat\n");
-}
-
-/*nel caso volessi stampare i risultati di ogni getto (non solo il punteggio
-ma anche i simboli estratti dai due giocatori) uso questa funzione
-per convertire il numero estratto in maniera casuale in un carattere che indica
-
-C= CARTA
-F=FORBICI
-S=SASSO
-
-*/
-char convert_into_string(int value){
-  char sign;
-  
-  if(value==1)
-    sign='C';
-  
-  if(value==2)
-    sign='F';
-  
-  
-  if(value==3)
-   sign='S';
-
-  return(sign);
 }
